fcc82860c7729598bed8b5d9efc95e08b08d50f6
psycho-baller <Ramim66809@gmail.com>
2022-04-10 23:12:25 -0600

    ACTUALLY fixed js problem w github.io

--- js/mainDong.js
+++ js/mainDong.js
@@ -1,25 +1,27 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const dongData = JSON.parse(fs.readFileSync('data\\ding_dong.json', 'utf8'));
 const usedDongIDs = new Set();
-const dongData = JSON.parse(fs.readFileSync('data\\ding_dong.json', 'utf8'));
 
 // get reference to button
 const dongJoke = document.getElementById("getDong");
 // add event listener for the button, for action "click"
 dongJoke.addEventListener("click", showDongJoke);
 function showDongJoke() {
-  const randomDong = dongData[Math.floor(Math.random() * dongData.length)];
-  if (!usedDongIDs.has(randomDong.id)) {
-    usedDongIDs.add(randomDong.id);
-    const dongJoke = `
-    Ding Dong!<br>
-    Who's there?<br>
-    ${randomDong.whos_there}<br>
-    ${randomDong.whos_there} who?<br>
-    ${randomDong.who}`;
-    document.getElementById("dongJoke").innerHTML = dongJoke;
-  } else if(usedDongIDs.size === dongData.length) {
-    document.getElementById("dongStatus").innerHTML = "Oh no! we're out of ding dong jokes, feel free to add new ones";
-  } else{
-    showDongJoke();
-  }
+  $.getJSON( '..\\data\\knock_knock.json', function(dongData) {
+    const randomDong = dongData[Math.floor(Math.random() * dongData.length)];
+    if (!usedDongIDs.has(randomDong.id)) {
+      usedDongIDs.add(randomDong.id);
+      const dongJoke = `
+      Ding Dong!<br>
+      Who's there?<br>
+      ${randomDong.whos_there}<br>
+      ${randomDong.whos_there} who?<br>
+      ${randomDong.who}`;
+      document.getElementById("dongJoke").innerHTML = dongJoke;
+    } else if(usedDongIDs.size === dongData.length) {
+      document.getElementById("dongStatus").innerHTML = "Oh no! we're out of ding dong jokes, feel free to add new ones";
+    } else{
+      showDongJoke();
+    }
+  });
 }
--- js/mainKnock.js
+++ js/mainKnock.js
@@ -1,29 +1,37 @@
-const fs = require('fs');
 const usedKnockIDs = new Set();
-const knockData = JSON.parse(fs.readFileSync('data\\knock_knock.json', 'utf8'));
 
 // get reference to button
 const knockJoke = document.getElementById("getKnock");
 // add event listener for the button, for action "click"
 knockJoke.addEventListener("click", showKnockJoke);
 function showKnockJoke() {
-  const randomKnock = knockData[Math.floor(Math.random() * knockData.length)];
-  if (!usedKnockIDs.has(randomKnock.id)) {
-    usedKnockIDs.add(randomKnock.id);
-    const knockJoke = `
-    Knock knock!<br>
-    Who's there?<br>
-    ${randomKnock.whos_there}<br>
-    ${randomKnock.whos_there} who?<br>
-    ${randomKnock.who}`;
-    document.getElementById("knockJoke").innerHTML = knockJoke;
-  } else if(usedKnockIDs.size === knockData.length) {
-    document.getElementById("knockStatus").innerHTML = "Oh no! we're out of knock-knock jokes, feel free to add new ones";
-  } else{
-    showKnockJoke();
-  }
+  $.getJSON( '..\\data\\knock_knock.json', function(knockData) {
+    const randomKnock = knockData[Math.floor(Math.random() * knockData.length)];
+
+    if (!usedKnockIDs.has(randomKnock.id)) {
+      usedKnockIDs.add(randomKnock.id);
+      const knockJoke = `
+      Knock Knock!<br>
+      Who's there?<br>
+      ${randomKnock.whos_there}<br>
+      ${randomKnock.whos_there} who?<br>
+      ${randomKnock.who}`;
+      document.getElementById("knockJoke").innerHTML = knockJoke;
+    } else if(usedKnockIDs.size === knockData.length) {
+      document.getElementById("knockStatus").innerHTML = "Oh no! we're out of knock-knock jokes, feel free to add new ones";
+    } else{
+      showKnockJoke();
+    }
+  });
 }
 
+
+
+// Perform other work here ...
+
+// $.getJSON("test.json", function(json) {
+//   console.log(json); // this will show the info it in firebug console
+// });
 
 // // get reference to button
 // var dark = document.getElementById("darkMode");
@@ -32,4 +40,3 @@ function showKnockJoke() {
 // function darkMode() {
 //   let element = document.body;
 //   element.classList.toggle("dark-mode");
-// }
